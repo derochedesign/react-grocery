1a19ead8724d77ba221fb6fb6764ef3193a30b80
Aaron <aaron@deroche.design>
2019-04-20 21:07:27 -0400

    final changes

--- js/main.js
+++ js/main.js
@@ -2,17 +2,25 @@ const { useState, useRef, useEffect, useContext} = React;
 
 const ListContext = React.createContext({});
 
+//Some notes:
+//Everything needs to refresh pretty much, which isnt great.
+//Also I think my solution to solving this was to store the entire context in a state at App level.. seems bad, but works.
+//Only thing that doesn't work, is when you delete an item, the qty sticks, which doesn't really make sense to me why but idk
+
 const ShoppingList = (props) => {
     
     console.log("refreshing ShoppingList");
-     
-    // const [qty, setQty] = useState([]);
+    let ctx = useContext(ListContext);
+    
+    const items = ctx.data;
+    const currCat = ctx.cat;
     
     return (
+        
         <ul className={props.classVal}>
         {
-            props.items.map( (item, i) => 
-                (props.currCat == item.cat || props.currCat == "all") && <ListItem key={i} i={i} item={item.val} id={item.id} cat={item.cat}/>
+            items.map( (item, i) => 
+                (currCat == item.cat || currCat == "all") && <ListItem key={i} i={i} item={item.val} id={item.id} cat={item.cat}/>
             )
         }
         </ul>
@@ -21,21 +29,21 @@ const ShoppingList = (props) => {
 
 const ListItem = props => {
     
-    let newQty = useContext(ListContext);
-    let thisItemIndex = newQty.data.findIndex(item => item.id == props.id);
+    let ctx = useContext(ListContext);
+    let thisItemIndex = ctx.data.findIndex(item => item.id == props.id)
     
-    console.log("refreshing ListItem");
-    
-    const [qty, setQty] = useState(newQty.data[thisItemIndex].qty);
-    newQty.data[thisItemIndex].qty = qty;
-    newQty.update();
+    const [qty, setQty] = useState(ctx.data[thisItemIndex].qty);
+    ctx.data[thisItemIndex].qty = qty;
+    ctx.updateLS();
     
     const deleteItem = () => {
-        let tempArr = newQty.data;
+        
+        let clone = {...ctx};
+        let tempArr = ctx.data;
         tempArr.splice([thisItemIndex], 1);
-        newQty.data = [...tempArr];
-        newQty.update();
-        console.log(newQty);
+        ctx.data = [...tempArr];
+        clone.data = [...tempArr];
+        ctx.update(clone);
         
     }
     
@@ -55,11 +63,22 @@ const Categories = props => {
     
     console.log("refreshing Categories");
     
+    const [currCat, setCurrCat] = useState("all");
+    const ctx = useContext(ListContext);
+    
+    const setCat = (val) => { 
+        let clone = {...ctx};
+        clone.cat = val;
+        ctx.cat = val;
+        setCurrCat(val);
+        ctx.update(clone);
+    }
+    
     return (
         <ul className={props.classVal}>
         {
-            props.allCat.map( (cat, i) => 
-                <Category key={i} i={i} currCat={props.currCat} cat={cat} setCat={props.setCat}/>
+            ctx.allCat.map( (cat, i) => 
+                <Category key={i} i={i} currCat={currCat} cat={cat} setCat={setCat}/>
             )
         }
         </ul>
@@ -77,25 +96,33 @@ const Category = props => {
         </li>
     )
 }
-const AddItem = props => {
+const AddItem = () => {
     
     console.log("refreshing AddItem");
     
     const newItemInput = useRef();
     
+    const ctx = useContext(ListContext);
+    const items = ctx.data;
+    const currCat = ctx.cat;
+    
     const handleSubmit = e => {
+        
         let randomID = Math.floor(Math.random() * 1000000000);
         //make sure to check all id's for dup
-        let newItem = {val: newItemInput.current.value, qty:1, cat: props.currCat, id:randomID};
-        let valid = props.items.filter( item => {
+        let newItem = {val: newItemInput.current.value, qty:1, cat: currCat, id:randomID};
+        let valid = items.filter( item => {
             let tempItem = newItem.val.toLowerCase();
             item.val = item.val.toLowerCase();
             return item.val == tempItem;
         });
         
         if (valid.length == 0) {
-            props.setItem([...props.items, newItem]);
+            let clone = {...ctx};
+            clone.data = [...items, newItem];
+            ctx.data = [...items, newItem];
             newItemInput.current.value = "";
+            ctx.update(clone);
         }
         else {
             alert("Already on the list!");
@@ -126,36 +153,41 @@ const App = () => {
     
     console.log("refreshing App");
     // let listData = JSON.parse(localStorage.getItem('list')) || [];
-    const [items, setItem] = useState([]);
-    
-    const [currCat, setCurrCat] = useState("all");
-    
+    let currCat = 'all';
     const categories = [`all`, `meat`, `prod`, `dairy`, `dry`];
     const title = "Shopping List";
     
-    useEffect(() => {
-        document.title = `Grocery List: ${items.length} Items`;
-    });
-    
     const updateLocalStorage = () => {
-        console.log("test");
-        
+        console.log("update ls");
     }
+    const updateApp = val => {
+        console.log("update app"); 
+        setTheStore(val);
+    }
+    
+    const [theStore, setTheStore] = useState({ data: [], allCat:categories, cat: currCat, update:updateApp, updateLS:updateLocalStorage});
+    // const items = [];
+    
+    //const [currCat, setCurrCat] = useState("all");
+    
+    useEffect(() => {
+        document.title = `Grocery List: ${theStore.data.length} Items`;
+    });
 
     return (
-      <ListContext.Provider value={{ data: items, update:updateLocalStorage}}>
+      <ListContext.Provider value={theStore}>
         <HeaderInfo theTitle={title}/>
 
         <form id="newItem" className="newitem" autocomplete="off">
             <label for="item" className="line-label">New Item</label>
-            <AddItem items={items} setItem={setItem} currCat={currCat} />
+            <AddItem />
         </form>
         
         <form id="filterCategories">
-            <Categories classVal="filters" currCat={currCat} allCat={categories} setCat={setCurrCat} />
+            <Categories classVal="filters" />
         </form>
         
-        <ShoppingList setItem={setItem} items={items} classVal="shoppinglist" currCat={currCat} setCat={setCurrCat} />
+        <ShoppingList classVal="shoppinglist" />
       </ListContext.Provider>
     );
 };
